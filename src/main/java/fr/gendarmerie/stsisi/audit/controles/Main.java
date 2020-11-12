7779aa48db5f67d11d90089a9b39408a503acb2a
Mathieu-94 <mathieu.leclerc94@gmail.com>
2020-11-12 10:40:09 +0100

    MAJ Thread

--- src/main/java/fr/gendarmerie/stsisi/audit/controles/Main.java
+++ src/main/java/fr/gendarmerie/stsisi/audit/controles/Main.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -18,19 +19,28 @@ public class Main {
         Date oDate = new Date();
         String date = dateFormat.format(oDate);
         List<IPlugins> mList = Arrays.asList(new GradleControl(), new FichierControl(), new DalvikControl(), new JavaControl());
+        ArrayList<Boolean> rList = new ArrayList<>();
         try {
             Files.walk(Paths.get("C:\\Users\\Shadow\\Desktop\\Test1")).filter(Files::isRegularFile).filter(Files::isReadable).filter(Files::isWritable).forEach((f) -> new Thread(() -> {
 //            Files.walk(Paths.get(path)).filter(Files::isRegularFile).filter(Files::isReadable).filter(Files::isWritable).forEach((f) -> new Thread(() -> {
-            for (IPlugins p : mList) {
-                if (p.controlName(f)) {
-                    if (p.controlSize(f)) {
-                        p.controlRegex(f, date);
+                for (IPlugins p : mList) {
+                    if (p.controlName(f)) {
+                        if (p.controlSize(f)) {
+                            rList.add(p.controlRegex(f, date));
+                        }
                     }
                 }
-            }
             }).start());
         } catch (Exception e) {
             System.out.println("Erreur parcours dossier => " + e);
+        }
+        while (Thread.activeCount() != 1) {
+            Thread.onSpinWait();
+        }
+        if (rList.contains(true)) {
+            System.exit(-1);
+        } else {
+            System.exit(0);
         }
     }
 }
